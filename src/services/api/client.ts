e48f84ef23b183eaa8def22d5126e6f4925b09b9
Ryan Waldon <ryanpwaldon@gmail.com>
2021-06-12 16:46:35 +1000

    feat: updates

--- src/services/api/client.ts
+++ src/services/api/client.ts
@@ -1,7 +1,7 @@
 import qs from 'qs'
 import axios from 'axios'
 
-export const client = axios.create({ baseURL: import.meta.env.VITE_SERVER_URL as string })
+export const client = axios.create({ baseURL: import.meta.env.VITE_SERVER_URL as string, withCredentials: true })
 
 client.interceptors.request.use(async (config) => {
   config.paramsSerializer = (params) => qs.stringify(params)
